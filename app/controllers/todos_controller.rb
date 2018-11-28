64f17445d25825486098e7010a185e76c01241c4
vivianafb <vivianafb@gmail.com>
2018-11-28 15:26:04 -0300

    Ruta show creada

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,4 +5,13 @@ class TodosController < ApplicationController
     def new
         @todo = Todo.new
     end
+    def create
+        @todo = Todo.new(todo_params)
+        @todo.save
+        redirect_to todos_path
+    end
+    private
+    def todo_params
+        params.require(:todo).permit(:description, :completed)
+    end
 end

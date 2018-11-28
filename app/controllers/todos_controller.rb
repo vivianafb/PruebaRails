c361966e9d8b942882f513e63f49c1962190d6d7
vivianafb <vivianafb@gmail.com>
2018-11-28 19:06:39 -0300

    Vista list creada

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -31,6 +31,9 @@ class TodosController < ApplicationController
         @todo.save
         redirect_to todos_path
     end
+    def list
+        @todos = Todo.all
+    end
 
     private
     def todo_params

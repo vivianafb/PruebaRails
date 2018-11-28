240c867efc7502f8015273b3587125db39636a74
vivianafb <vivianafb@gmail.com>
2018-11-28 16:31:17 -0300

    Metodo destroy creado

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'todos/:id', to: 'todos#show', as: 'todo'
   get 'todos/:id/edit', to: 'todos#edit'
   patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to:'todos#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

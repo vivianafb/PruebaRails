f10c06b286709c6303b9a155da1358bd5c860698
vivianafb <vivianafb@gmail.com>
2018-11-28 15:30:39 -0300

    Controller show creado

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
   get 'todos/new'
   post 'todos', to: 'todos#create'
+  get 'todos/:id', to 'todos#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

144a04ef533851ae205f23417bdb5498ffe304ae
vivianafb <vivianafb@gmail.com>
2018-11-28 16:01:56 -0300

    Controller edit creado

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'todos/new'
   post 'todos', to: 'todos#create'
   get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

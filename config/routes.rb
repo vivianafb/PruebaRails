df577c25563190ea29cc73cc96f424a41964b63a
vivianafb <vivianafb@gmail.com>
2018-11-29 19:58:24 -0300

    Arreglo del link list

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'todos/new'
   post 'todos', to: 'todos#create'
   get 'todos/:id', to: 'todos#show', as: 'todo'
-  get 'todos/:id/edit', to: 'todos#edit'
+  get 'todos/:id/edit', to: 'todos#edit', as:'edit'
   patch 'todos/:id', to: 'todos#update'
   delete 'todos/:id', to:'todos#destroy'
   get 'todos/:id/complete', to: 'todos#complete'

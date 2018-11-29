5c90cb16b86653c08a35d59d5b3e7a962ca2d775
vivianafb <vivianafb@gmail.com>
2018-11-29 20:10:53 -0300

    Vista list arreglada

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   patch 'todos/:id', to: 'todos#update'
   delete 'todos/:id', to:'todos#destroy'
   get 'todos/:id/complete', to: 'todos#complete'
-  get 'todos/list'
+  get 'list', to: 'todos#list'
+  root 'todos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
